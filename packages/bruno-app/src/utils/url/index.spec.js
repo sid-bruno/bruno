2818f155c993ad0c700d8c8b65810f89c2fffa48
Anton <anton@trugen.net>
2025-09-23 09:01:00 +0200

    Support for Odata style path params (#5048)
    * Support for Odata style path params
    
    * Remove test statement
    
    * Update packages/bruno-app/src/utils/url/index.spec.js
    
    Add more testcases
    
    Co-authored-by: sid-bruno <siddharth@usebruno.com>
    
    * Performance improvements
    
    * Add testcases for odata style url params
    
    ---------
    
    Co-authored-by: sid-bruno <siddharth@usebruno.com>

--- packages/bruno-app/src/utils/url/index.spec.js
+++ packages/bruno-app/src/utils/url/index.spec.js
@@ -48,7 +48,7 @@ describe('Url Utils - parsePathParams', () => {
     const params = parsePathParams("https://example.com/ExchangeRates(':ExchangeRateOID')");
     expect(params).toEqual([{ name: 'ExchangeRateOID', value: '' }]);
   });
-  
+
   it('should parse path param inside parentheses and no quotes', () => {
     const params = parsePathParams("https://example.com/ExchangeRates(:ExchangeRateOID)");
     expect(params).toEqual([{ name: 'ExchangeRateOID', value: '' }]);
@@ -56,12 +56,12 @@ describe('Url Utils - parsePathParams', () => {
 
   it('should parse multiple path params inside parentheses', () => {
     const params = parsePathParams("https://example.com/Exchange(:ExchangeId)/ExchangeRates(:ExchangeRateOID)");
-    expect(params).toEqual([{ name: 'ExchangeId', value: '' },{ name: 'ExchangeRateOID', value: '' }]);
+    expect(params).toEqual([{ name: 'ExchangeId', value: '' }, { name: 'ExchangeRateOID', value: '' }]);
   });
 
   it('should parse mix and match of normal and param inside parentheses', () => {
     const params = parsePathParams("https://example.com/Exchange(:ExchangeId)/:key");
-    expect(params).toEqual([{ name: 'ExchangeId', value: '' },{ name: 'key', value: '' }]);
+    expect(params).toEqual([{ name: 'ExchangeId', value: '' }, { name: 'key', value: '' }]);
   });
 
   // OData-specific test cases for enhanced path parameter parsing
